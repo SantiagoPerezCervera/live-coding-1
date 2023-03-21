85440763e801aeecff185eb98c057f01d220968a
SantiagoPerez <sany.pc26@gmail.com>
2023-03-21 16:41:32 -0600

    ejercicio de liveCoding

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// Escribe tu código aquí.
+function resultadoadoarregloay(string, arreglo) {
+    let resultado = [];
+    for (let i = 0; i < arreglo.length; i++) {
+      if (arreglo[i].length > string.length) {
+        resultado.push(arreglo[i]);
+      }
+    }
+    return resultado;
+  }
+
+  const myarreglo = ["HolaMundo", "Hola Mundo", "Bootcamp", "Ejemplo", "Personal"];
+  const string = 'estadio';
+  
+  console.log(resultadoadoarregloay(string, myarreglo)); 
+
+
+  //Mi compañero fue carlos zarate, me realizo la observacion del tiempo que me tardo en realizar el codigo, no pude terminarlo
+  //solo hice el ejercicio 1
